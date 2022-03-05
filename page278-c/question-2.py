51394ff70e88bb3efd589c9014320e50870c0667
CodyAaTherf <aatherfgamerplayzs@gmail.com>
2022-03-05 16:40:38 +0530

    Solved question 2-p278-c

---
+++ page278-c/question-2.py
@@ -0,0 +1,10 @@
+# Write a program where the multiplication and division of two arbitatry numbers is get printed.
+
+x = float(input("Enter first number: "))
+y = float(input("Enter second number: "))
+
+q = x * y
+r = x / y
+
+print(f"{x} * {y} = {q}")
+print(f"{x} / {y} = {r}")
