d7b13a53ecf6b593d98908b3a4cd37f5dc62564e
CodyAaTherf <aatherfgamerplayzs@gmail.com>
2022-03-05 15:36:47 +0530

    Solved question 2-p278-b

---
+++ page278-b/question-2.py
@@ -0,0 +1,9 @@
+# Create a python program to print the factorial of 10
+
+num = 10
+factorial = 1
+
+for i in range(1 , num + 1):
+    factorial = factorial*i
+
+print(f"The factorial of {num} is {factorial}")
