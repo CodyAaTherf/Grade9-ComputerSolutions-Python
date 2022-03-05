e2b6717a53bcf899397698f99593323f11002222
CodyAaTherf <aatherfgamerplayzs@gmail.com>
2022-03-05 15:28:46 +0530

    Solved Question 1-p278-b

---
+++ page278-b/question-1.py
@@ -0,0 +1,6 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+sum = a + b + c
+
+print(f"The sum of {a} , {b} and {c} is {sum}")
