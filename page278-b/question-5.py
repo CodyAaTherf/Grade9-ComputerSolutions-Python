3ccb81b02e9d8d9e3e1da4aa3460c28a5a7e4ac8
CodyAaTherf <aatherfgamerplayzs@gmail.com>
2022-03-05 15:51:44 +0530

    Solved question 5-p278-b
    # hehe be efficient and not just do `print(list1[4])` , `print(list1[3])` etc etc

---
+++ page278-b/question-5.py
@@ -0,0 +1,8 @@
+# Create a python list containing first 5 positive even numbers. Display the list elements in the desending order.
+
+list1 = [2, 4, 6, 8, 10]
+
+list1.sort(reverse=True)
+print(list1)
+
+# hehe be efficient and not just do `print(list1[4])` , `print(list1[3])` etc etc
