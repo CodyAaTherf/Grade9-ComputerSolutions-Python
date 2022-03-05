c09e12fceca6bfd0253cd8b131b5a00a2f3e93a5
CodyAaTherf <aatherfgamerplayzs@gmail.com>
2022-03-05 16:50:37 +0530

    Solved Question 4 & 5 p278-c

---
+++ page278-c/question-3.py
@@ -0,0 +1,5 @@
+# Write a program to convert km to meters
+
+k = float(input("Enter the distance in km: "))
+m = k * 1000
+print(f"{k} km = {m} m")
---
+++ page278-c/question-4.py
@@ -0,0 +1,6 @@
+# Write a program to calculate the area of a rectangle with arbitrary values of length and breadth
+
+l = float(input("Enter length: "))
+b = float(input("Enter breadth: "))
+a = l * b
+print(f"Area of rectangle = {a}")
