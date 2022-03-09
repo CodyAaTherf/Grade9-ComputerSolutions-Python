f9d1087508a17d88ccdc7b9581e772089d7db901
CodyAaTherf <aatherfgamerplayzs@gmail.com>
2022-03-09 20:04:18 +0530

    Solved qn10-p278-c

---
+++ page278-c/question-10.py
@@ -0,0 +1,7 @@
+# Write a program to find the population density of a region
+
+population = int(input("Enter the population: "))
+area = float(input("Enter the area: "))
+density = population / area
+
+print(f"{population} / {area} = {density}")
