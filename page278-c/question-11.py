4c753a37394683cf4f184bfcc30f10b2c0801dbf
CodyAaTherf <aatherfgamerplayzs@gmail.com>
2022-03-09 20:06:19 +0530

    Solved qn11-p278-c

---
+++ page278-c/question-11.py
@@ -0,0 +1,7 @@
+# Write a program to find the mileage of a vehicle
+
+distance = float(input("Enter the distance: "))
+fuel = float(input("Enter the fuel: "))
+mileage = distance / fuel
+
+print(f"{distance} / {fuel} = {mileage}")
