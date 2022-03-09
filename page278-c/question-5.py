cb2599592a41728de1a5726c050d9fcac22a3dce
CodyAaTherf <aatherfgamerplayzs@gmail.com>
2022-03-09 19:53:06 +0530

    Solved qns 6&7 p278-c

---
+++ page278-c/question-5.py
@@ -0,0 +1,5 @@
+# Write a program to convert Kelvin to Celsius
+
+k = float(input("Enter the temperature in Kelvin: "))
+c = k - 273.15
+print(f"{k} K = {c} C")
---
+++ page278-c/question-6.py
@@ -0,0 +1,5 @@
+# Write a program to convert Celsius to Fahrenheit
+
+c = float(input("Enter the temperature in Celsius: "))
+f = c * 9/5 + 32
+print(f"{c} C = {f} F")
