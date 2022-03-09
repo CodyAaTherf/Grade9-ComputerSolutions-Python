31638f2ae483dea25f245b9069a041f57d37d7a4
CodyAaTherf <aatherfgamerplayzs@gmail.com>
2022-03-09 20:00:18 +0530

    Solved qn8-278-c

---
+++ page278-c/question-8.py
@@ -0,0 +1,6 @@
+# Write a program to calculate the voltage for a given values of resistance and current
+
+resistance = float(input("Enter the resistance: "))
+current = float(input("Enter the current: "))
+voltage = resistance * current
+print(f"{voltage} V")
